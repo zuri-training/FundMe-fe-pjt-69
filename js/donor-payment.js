8c15a597e6f529fe7a8624c34b54c05acb88a2a0
Charles Obuzor <charlesobuzor@outlook.com>
2021-07-06 15:41:24 +0100

    rm header, edit .js

--- js/donor-payment.js
+++ js/donor-payment.js
@@ -1,8 +1,10 @@
+// this function displays the payment section after clicking the continue button
 function showDiv() {
   document.getElementById('payment').style.display = "block";
   document.getElementById('continue').style.display = "none";
 }
 
+// this part, sets the value of the form to the amount click on from the 4 availble buttons
 var value_1 = document.getElementById('value-1');
 var value_2 = document.getElementById('value-2');
 var value_3 = document.getElementById('value-3');
@@ -10,18 +12,10 @@ var value_4 = document.getElementById('value-4');
 
 var amount_input = document.getElementById('amount');
 
-var percent_2 = document.getElementById('percent-2');
-var percent_5 = document.getElementById('percent-5');
-var percent_8 = document.getElementById('percent-8');
-var percent_10 = document.getElementById('percent-10');
-
-value_2.innerHTML = 2000;
 value_1.innerHTML = 1000;
+value_2.innerHTML = 2000;
 value_3.innerHTML = 10000;
 value_4.innerHTML = 50000;
-
-
-var amount_donated = document.getElementById('amount-donated');
 
 
 function amount(value_1) {
@@ -40,4 +34,5 @@ function amount(value_4) {
   amount_input.value = value_4.innerHTML;
 }
 
+// that's all.
 
